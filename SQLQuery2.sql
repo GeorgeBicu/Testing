7884fb6727fb93912c5e1bc9cd5f992df7a51029
GeorgeBicu <122690525+GeorgeBicu@users.noreply.github.com>
2023-01-14 19:15:40 +0200

    Create SQLQuery2.sql
    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,19 @@
+SELECT COUNT(IDTranzactie) AS Tranzactii
+FROM Tranzactii
+WHERE IDClient=1
+
+SELECT COUNT(IDTranzactie) AS Tranzactii
+FROM Tranzactii
+WHERE IDClient=1 AND IDProdus=123
+
+SELECT COUNT(IDTranzactie) AS Tranzactii
+FROM Tranzactii
+WHERE IDClient=1 AND IDProdus=124
+
+SELECT COUNT(IDTranzactie) AS ProduseCumparate
+FROM Tranzactii
+WHERE IDProdus=124
+
+SELECT COUNT(IDTranzactie) AS ProduseCumparate
+FROM Tranzactii
+WHERE IDProdus=123
